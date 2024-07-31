50b25386797b2c7c0bb05d4f78676d2ec1df7306
Jatin Bohra <91942675+jatinbohra@users.noreply.github.com>
2024-07-31 18:06:28 +0530

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -1,3 +1,4 @@
+2. End-of-file
 /*The challenge here is to read  lines of input until you reach EOF, then number and print all  lines of content.
 
 Hint: Java's Scanner.hasNext() method is helpful for this problem.
